1b978b7904f723d55c346ccc02810871d2b2c7bf
Dmitriyliasha <lyasha@ukr.net>
2018-01-29 01:02:45 +0200

    Добавили еще и functions.php

---
+++ www/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дмитрий
+ * Date: 29.01.2018
+ * Time: 1:01
+ */
--- www/index.php
+++ www/index.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Дмитрий
- * Date: 29.01.2018
- * Time: 0:41
- */
+echo phpinfo();
